1c17064e05825074c553bf3397727af9d4b4f662
Your Name <you@example.com>
2021-01-07 20:57:31 +0100

    sz

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Style.css">
+
+
+    </head>
+    <body>
+        
+
+
+
+
+
+
+    </body>
+
+
+</html>
