8244a4bd064b1f86309e686008aa5f63ee99d2ff
Your Name <you@example.com>
2021-01-13 17:26:28 +0100

    nouvells modifs

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 
+
+
+
+
 ?>
-
-<!DOCTYPE html>
-<html>
-
-    <head>
-
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="Style.css">
-
-
-    </head>
-    <body>
-        
-
-
-
-
-
-
-    </body>
-
-
-</html>
