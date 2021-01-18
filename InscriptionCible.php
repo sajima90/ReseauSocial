993ca14db85cda9815fcb5503ef8e4ebddd2266e
Sajima <73099815+sajima90@users.noreply.github.com>
2021-01-18 10:36:00 +0100

    Update InscriptionCible.php

--- InscriptionCible.php
+++ InscriptionCible.php
@@ -1,5 +1,7 @@
 <p>Bonjour !</p>
 
 <p>Je sais comment tu t'appelles, hé hé. Ton mot de passe est <?php echo $_POST['password']; ?> !</p>
+    ton nom est <?php echo $_POST['speudo']; ?>
+    ton adresse mail est <?php echo $_POST['email']; ?>
 
 <p>Si tu veux changer de mot de passe, <a href="inscription.php">clique ici</a> pour revenir à la page formulaire.</p>
