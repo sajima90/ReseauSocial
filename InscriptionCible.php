6b371eeb4cd810cfc739a6424af626643f2890f5
Sajima <73099815+sajima90@users.noreply.github.com>
2021-01-18 11:01:18 +0100

    Update InscriptionCible.php

--- InscriptionCible.php
+++ InscriptionCible.php
@@ -6,3 +6,23 @@
     ton adresse mail est <?php echo $_POST['email']; ?>
 
 <p>Si tu veux changer de mot de passe, <a href="inscription.php">clique ici</a> pour revenir à la page formulaire.</p>
+
+
+
+
+
+
+
+
+<?php
+$monfichier = fopen('compteur.txt', 'r+');
+ 
+$pages_vues = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
+$pages_vues += 1; // On augmente de 1 ce nombre de pages vues
+fseek($monfichier, 0); // On remet le curseur au début du fichier
+fputs($monfichier, $pages_vues); // On écrit le nouveau nombre de pages vues
+ 
+fclose($monfichier);
+ 
+echo '<p>Cette page a été vue ' . $pages_vues . ' fois !</p>';
+?>
