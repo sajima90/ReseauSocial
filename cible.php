3007cb814dc122fb411ad6f782316629f5902304
Sajima <73099815+sajima90@users.noreply.github.com>
2021-01-18 10:21:09 +0100

    Create cible.php

---
+++ cible.php
@@ -0,0 +1,5 @@
+<p>Bonjour !</p>
+
+<p>Je sais comment tu t'appelles, hé hé. Ton mot de passe est <?php echo $_POST['prenom']; ?> !</p>
+
+<p>Si tu veux changer de mot de passe, <a href="inscription.html">clique ici</a> pour revenir à la page formulaire.</p>
