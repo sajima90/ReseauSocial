242e4f4bcafc4969c482f69506de0169f691b9e3
Sajima <73099815+sajima90@users.noreply.github.com>
2021-01-19 12:42:32 +0100

    Update cible.php

--- cible.php
+++ cible.php
@@ -3,4 +3,4 @@
 <p>Je sais comment tu t'appelles, hé hé. Ton mot de passe est <?php echo $_POST['password']; ?> !</p>
 
 <p>Si tu veux changer de mot de passe, <a href="inscription.php">clique ici</a> pour revenir à la page formulaire.</p>
-<p>Si tu veux changer de mot de passe, <a href="profile.html">clique ici</a> pour revenir à la page formulaire.</p>
+<p><a href="profile.html">clique ici</a></p>
