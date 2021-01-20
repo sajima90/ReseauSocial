9281f4e2da59900700553f2e10dc2e552747c7b7
Sajima <73099815+sajima90@users.noreply.github.com>
2021-01-20 08:20:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -70,6 +70,7 @@
                 <div class="register">
 
                     <p>Pas de compte ? <a href="inscription.php" class="f"> Inscrivez vous !</a><p>
+                    <p>Pas de compte ? <a href="https://github.com/sajima90/ReseauSocial" class="f"> Sajima-Thib76</a><p>
 
 
                 </div>
